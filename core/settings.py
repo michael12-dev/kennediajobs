a709c843f60cd60f9721b953438e71b3ebffe959
Michael <115664308+michael12-dev@users.noreply.github.com>
2026-06-18 19:59:54 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL'),
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=False,
     )
 }
 
