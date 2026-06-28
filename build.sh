b9db1d31f474c321688e4b1f0ec5dd7f512ef8a3
Michael <115664308+michael12-dev@users.noreply.github.com>
2026-06-28 12:06:40 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py createcachetable
 python manage.py create_superadmin
